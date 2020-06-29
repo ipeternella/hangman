9cfb8de3d50a3423131ed30ce410599ca1ed4bee
IgooorGP <igor.feq@gmail.com>
2020-06-29 20:24:00 -0300

    Added appropriate DockerCompose launch profile.

--- start-from-compose.sh
+++ start-from-compose.sh
@@ -9,4 +9,4 @@ echo "[PRE-RUN]: Building project..."
 dotnet build $CSPROJ_PATH
 
 echo "[PRE-RUN]: Watching and running project..."
-dotnet watch --project $CSPROJ_PATH run --urls https://$ASPNET_SOCKET_BIND_ADDRESS:$ASPNET_SOCKET_BIND_PORT --launch-profile=DockerCompose
+dotnet watch --project $CSPROJ_PATH run --urls https://$ASPNETCORE_SOCKET_BIND_ADDRESS:$ASPNETCORE_SOCKET_BIND_PORT --launch-profile=DockerCompose
