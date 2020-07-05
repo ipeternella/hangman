563960e70f716b30dc87fed2c161885c584017cd
IgooorGP <igor.feq@gmail.com>
2020-07-05 16:04:59 -0300

    Removed unnecessary manual migration from start-from-compose.sh.

--- start-from-compose.sh
+++ start-from-compose.sh
@@ -2,9 +2,6 @@
 PROJECT_NAME=Hangman
 CSPROJ_PATH=./$PROJECT_NAME/$PROJECT_NAME.csproj
 
-echo "[PRE-RUN]: Running migrations..."
-dotnet ef database update --project $CSPROJ_PATH  # reads ASPNETCORE_ENVIRONMENT
-
 echo "[PRE-RUN]: Building project..."
 dotnet build $CSPROJ_PATH
 
