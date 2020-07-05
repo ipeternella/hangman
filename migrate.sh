c3ff858d22cc41bcf57b64dd11ae0bfbccc059fb
IgooorGP <igor.feq@gmail.com>
2020-07-05 16:15:39 -0300

    Removed some old unnecessary files.

--- migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PROJECT_NAME=Hangman
-CSPROJ_PATH=./$PROJECT_NAME/$PROJECT_NAME.csproj
-
-echo "[MIGRATION]: Running migrations..."
-dotnet ef database update --project $CSPROJ_PATH  # reads ASPNETCORE_ENVIRONMENT
