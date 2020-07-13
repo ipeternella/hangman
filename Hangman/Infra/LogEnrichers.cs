483c6558ed34bbc2b432a2d189e93acd1c876cac
IgooorGP <igor.feq@gmail.com>
2020-07-13 20:50:21 -0300

    Removed appsettings.DockerCompose.json due to env vars overrides.

---
+++ Hangman/Infra/LogEnrichers.cs
@@ -0,0 +1,7 @@
+namespace Hangman.Infra
+{
+    public class LogEnrichers
+    {
+        
+    }
+}
--- start-from-compose.sh
+++ start-from-compose.sh
@@ -6,4 +6,4 @@ echo "[PRE-RUN]: Building project..."
 dotnet build $CSPROJ_PATH
 
 echo "[PRE-RUN]: Watching and running project..."
-dotnet watch --project $CSPROJ_PATH run --urls https://$ASPNETCORE_SOCKET_BIND_ADDRESS:$ASPNETCORE_SOCKET_BIND_PORT --launch-profile=DockerCompose
+dotnet watch --project $CSPROJ_PATH run --urls https://$ASPNETCORE_SOCKET_BIND_ADDRESS:$ASPNETCORE_SOCKET_BIND_PORT
