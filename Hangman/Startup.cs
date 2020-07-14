dbcf3ca017c0616eac5ff020b147c763d7b8856f
IgooorGP <igor.feq@gmail.com>
2020-07-14 10:50:03 -0300

    Added healthcheck endpoint and UI dashboard.

--- Hangman/Startup.cs
+++ Hangman/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Hangman.Repository;
 using Hangman.Repository.Interfaces;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -27,14 +29,19 @@ namespace Hangman
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // PostgreSQL healthcheck
+            // injection of health-checking services (database, cache, etc. goes here)
             services.AddHealthChecks()
                 .AddNpgSql(Configuration.GetConnectionString("DBConnection"));
-            
-            //services.AddHealthChecksUI();
 
+            // injection of the friendly health-check UI service (requires DB for health history persistence)
+            // configuration of endpoints for the UI client on the 'HealthChecksUI' of appsettings.json
+            services.AddHealthChecksUI()
+                .AddPostgreSqlStorage(Configuration.GetConnectionString("DBConnection"));
+            
+            // injection os other services
             services.AddHttpContextAccessor()
-                .AddDbContext<HangmanDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DBConnection")))
+                .AddDbContext<HangmanDbContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("DBConnection")))
                 .AddScoped(typeof(IHangmanRepositoryAsync<>), typeof(HangmanRepositoryAsync<>)) // generic repository
                 .AddScoped<IGameRoomServiceAsync, GameRoomServiceAsync>()
                 .AddControllers();
@@ -49,29 +56,37 @@ namespace Hangman
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseHttpsRedirection();
-            
-            // healthcheck dashboard
-            // app.UseHealthChecksUI(options => options.UIPath = "healthcheck-dashboard");
-            
-            // Reduces a lot of logging boilerplate
+
+            // middleware for condensing many access log lines into a SINGLE useful one
             app.UseSerilogRequestLogging();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // middleware for activating the healthcheck UI
+            app.UseHealthChecksUI(options => options.UIPath = "/healthcheck-dashboard");
             
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHealthChecks("/health");
+                // changes health-check endpoint to return a JSON rather than plain/text 'Healthy'
+                // this endpoint must match the endpoint of 'HealthChecksUI' URI
+                endpoints.MapHealthChecks("/healthcheck", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
                 endpoints.MapControllers();
             });
-            
+
             // Migrations and seed db (when in development/compose ONLY)
-            Migrate(app, logger, executeSeedDb: env.IsDevelopment() || env.IsEnvironment("Local") ||  env.IsEnvironment("DockerCompose"));
+            Migrate(app, logger,
+                executeSeedDb: env.IsDevelopment() || env.IsEnvironment("Local") || env.IsEnvironment("DockerCompose"));
         }
-        
+
         /**
          * Applies possible missing migrations from the database.
          */
@@ -79,7 +94,7 @@ namespace Hangman
         {
             using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
             using var context = serviceScope.ServiceProvider.GetService<HangmanDbContext>();
-            
+
             // always execute possible missing migrations
             if (context.Database.GetPendingMigrations().ToList().Any())
             {
@@ -89,7 +104,7 @@ namespace Hangman
 
             // seeding DB only when asked
             if (!executeSeedDb) return;
-            
+
             logger.LogInformation("Seeding the database...");
             SeedDb(context, logger);
         }
@@ -99,11 +114,12 @@ namespace Hangman
          */
         private static void SeedDb(HangmanDbContext context, ILogger<Startup> logger)
         {
-            if (context.GameRooms.Any()) {
+            if (context.GameRooms.Any())
+            {
                 logger.LogInformation("Database has already been seeded. Skipping it...");
                 return;
             }
-            
+
             logger.LogInformation("Saving entities...");
             var gameRooms = new List<GameRoom>
             {
@@ -112,7 +128,7 @@ namespace Hangman
                 new GameRoom {Name = "Game Room 3"}
             };
             context.AddRange(gameRooms);
-            
+
             logger.LogInformation("Database has been seeded successfully.");
             context.SaveChanges();
         }
