7ed34754229d2f559f204ab5b5ca319a0c42cf77
IgooorGP <igor.feq@gmail.com>
2020-07-18 17:58:04 -0300

    Added game room creation test.

--- Tests/Hangman/Integration/GameRoomsTests.cs
+++ Tests/Hangman/Integration/GameRoomsTests.cs
@@ -1,5 +1,9 @@
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Hangman.Application;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Tests.Hangman.Support;
 using HangmanProject = Hangman;
@@ -21,11 +25,39 @@ namespace Tests.Hangman.Integration
             // act
             var response = await Client.GetAsync(url);
             var responseAsString = await response.Content.ReadAsStringAsync();
-            var responseAsJson = JArray.Parse(responseAsString);  // JObject if response is not an array
+            var responseAsJson = JArray.Parse(responseAsString); // JObject if response is not an array
 
             // assert
             Assert.Equal(3, responseAsJson.Count);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory(DisplayName = "Should POST to /api/v1/gameroom and GET api/v1/gameroom/id to get an HTTP 200")]
+        [InlineData("/api/v1/gameroom")]
+        public async Task TestShouldMakeRequestToGameRoomAndGetHttp200AfterCreation(string url)
+        {
+            // arrange
+            var postData = JsonConvert.SerializeObject(new
+            {
+                Name = "Game Room Test"
+            });
+            var payload = new StringContent(postData, Encoding.UTF8, "application/json");
+
+            // act
+            var postResponse = await Client.PostAsync(url, payload);
+            var postResponseAsJson = JObject.Parse(await postResponse.Content.ReadAsStringAsync());
+            var createdGameRoomId = postResponseAsJson["id"].ToString();
+            var createdGameRoomName = postResponseAsJson["name"].ToString();
+
+            // assert
+            Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+            Assert.Equal("Game Room Test", createdGameRoomName); // expected game room
+
+            var getResponse = await Client.GetAsync(url + $"/{createdGameRoomId}");
+            var getResponseAsJson = JObject.Parse(await getResponse.Content.ReadAsStringAsync());
+
+            Assert.Equal(createdGameRoomId, getResponseAsJson["id"]);
+            Assert.Equal(createdGameRoomName, getResponseAsJson["name"]);
         }
     }
 }
