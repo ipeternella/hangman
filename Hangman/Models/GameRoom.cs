873ab9dc2fcffe5576a5f9ed7c380e0574c713fb
IgooorGP <igor.feq@gmail.com>
2020-07-18 23:31:44 -0300

    Created User model and GameRoomUser many-to-many model.

--- Hangman/Models/GameRoom.cs
+++ Hangman/Models/GameRoom.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Hangman.Models
@@ -8,5 +9,39 @@ namespace Hangman.Models
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
+        
+        // fk to join table for many-to-many relationships to access Users
+        public ICollection<GameRoomUser> GameRoomUsers { get; set; }
     }
+    
+    public class User : BaseEntity
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        
+        // fk to join table for many-to-many relationships to access GameRooms
+        public ICollection<GameRoomUser> GameRoomUsers { get; set; }
+    }
+    
+    public class GameRoomUser : BaseEntity
+    {
+        [Required]
+        public Guid GameRoomId { get; set; }
+        
+        [Required]
+        public GameRoom GameRoom { get; set; }
+        
+        [Required]
+        public User User { get; set; }
+        
+        [Required]
+        public User UserId { get; set; }
+        
+        [Required]
+        public bool IsHost { get; set; }
+        
+        [Required]
+        public bool IsBanned { get; set; }
+    } 
 }
--- Hangman/Repository/HangmanDbContext.cs
+++ Hangman/Repository/HangmanDbContext.cs
@@ -10,14 +10,16 @@ namespace Hangman.Repository
      * The Database context of this application. Contains all DbSets used for operations.
      * Adds CreatedAt and UpdatedAt fields.
      */
-    public class HangmanDbContext: DbContext
+    public class HangmanDbContext : DbContext
     {
         public HangmanDbContext(DbContextOptions<HangmanDbContext> options) : base(options)
         {
         }
-        
+
         public DbSet<GameRoom> GameRooms { get; set; }
-        
+        public DbSet<User> Users { get; set; }
+        public DbSet<GameRoomUser> GameRoomUsers { get; set; }
+
         /**
          * Saves the context to the database. Adds CreatedAt and UpdatedAt.
          */
@@ -26,7 +28,7 @@ namespace Hangman.Repository
             AutomaticallyAddCreatedAndUpdatedAt();
             return base.SaveChanges();
         }
-        
+
         /**
          * Saves the context to the database (Async version).
          */
@@ -35,20 +37,36 @@ namespace Hangman.Repository
             AutomaticallyAddCreatedAndUpdatedAt();
             return base.SaveChangesAsync(cancellationToken);
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GameRoomUser>()
+                .HasKey(gameRoomUser => new {gameRoomUser.GameRoomId, gameRoomUser.UserId});
+
+            modelBuilder.Entity<GameRoomUser>()
+                .HasOne(gameRoomUser => gameRoomUser.User)
+                .WithMany(user => user.GameRoomUsers)
+                .HasForeignKey(gameRoomUser => gameRoomUser.UserId);
+
+            modelBuilder.Entity<GameRoomUser>()
+                .HasOne(gameRoomUser => gameRoomUser.GameRoom)
+                .WithMany(gameRoom => gameRoom.GameRoomUsers)
+                .HasForeignKey(gameRoomUser => gameRoomUser.GameRoomId);
+        }
+
         private void AutomaticallyAddCreatedAndUpdatedAt()
         {
             var entitiesOnDbContext = ChangeTracker.Entries<BaseEntity>();
-            
-            if (entitiesOnDbContext == null) return;  // nothing was changed on DB context
-            
+
+            if (entitiesOnDbContext == null) return; // nothing was changed on DB context
+
             // createdAt addition
             foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Added))
             {
                 item.Entity.CreatedAt = System.DateTime.Now;
                 item.Entity.UpdatedAt = System.DateTime.Now;
             }
-                
+
             // updatedAt addition
             foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Modified))
             {
