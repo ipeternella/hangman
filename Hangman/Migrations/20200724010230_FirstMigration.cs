1a28c9a2336b3fb0f3a7f20acb31d33575ad142b
IgooorGP <igor.feq@gmail.com>
2020-07-23 22:11:28 -0300

    Added new initial migration.

---
+++ Hangman/Migrations/20200724010230_FirstMigration.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hangman.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameRooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameRoomPlayers",
+                columns: table => new
+                {
+                    GameRoomId = table.Column<Guid>(nullable: false),
+                    PlayerId = table.Column<Guid>(nullable: false),
+                    Id = table.Column<Guid>(nullable: false, defaultValue: new Guid("096938ab-7f67-4c41-8cb3-8cccc4d4c947")),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsHost = table.Column<bool>(nullable: false),
+                    IsBanned = table.Column<bool>(nullable: false),
+                    IsInRoom = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameRoomPlayers", x => new { x.GameRoomId, x.PlayerId });
+                    table.ForeignKey(
+                        name: "FK_GameRoomPlayers_GameRooms_GameRoomId",
+                        column: x => x.GameRoomId,
+                        principalTable: "GameRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameRoomPlayers_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameRoomPlayers_PlayerId",
+                table: "GameRoomPlayers",
+                column: "PlayerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameRoomPlayers");
+
+            migrationBuilder.DropTable(
+                name: "GameRooms");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
