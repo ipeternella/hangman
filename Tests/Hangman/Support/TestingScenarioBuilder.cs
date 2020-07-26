98a1a0dd9e0cdbb9d0b98ac6de2dc3c0d690a9f7
IgooorGP <igor.feq@gmail.com>
2020-07-25 23:54:06 -0300

    Added new test scenario with a player inside a game room.

--- Tests/Hangman/Support/TestingScenarioBuilder.cs
+++ Tests/Hangman/Support/TestingScenarioBuilder.cs
@@ -9,13 +9,14 @@ namespace Tests.Hangman.Support
     public class TestingScenarioBuilder
     {
         private readonly HangmanDbContext _context;
-        
+
         public TestingScenarioBuilder(HangmanDbContext context)
         {
             _context = context;
         }
 
-        public async Task<List<GameRoom>> BuildScenarioWithThreeRooms(string name1 = "Room 1", string name2 = "Room 2", string name3 = "Room 3")
+        public async Task<List<GameRoom>> BuildScenarioWithThreeRooms(string name1 = "Room 1", string name2 = "Room 2",
+            string name3 = "Room 3")
         {
             var gameRooms = new List<GameRoom>()
             {
@@ -23,13 +24,13 @@ namespace Tests.Hangman.Support
                 new GameRoom {Name = "Game Room 2"},
                 new GameRoom {Name = "Game Room 3"}
             };
-            
+
             await _context.AddRangeAsync(gameRooms);
             await _context.SaveChangesAsync();
 
             return gameRooms;
         }
-        
+
         public async Task<Tuple<List<GameRoom>, List<Player>>> BuildScenarioWithThreeRoomsAndThreePlayers()
         {
             var players = new List<Player>()
@@ -44,6 +45,28 @@ namespace Tests.Hangman.Support
             var gameRooms = await BuildScenarioWithThreeRooms();
 
             return Tuple.Create(gameRooms, players);
+        }
+
+        public async Task<Tuple<GameRoom, Player, GameRoomPlayer>> BuildScenarioWithAPlayerInRoom(
+            bool isInRoom = true, bool isHost = false, bool isBanned = false)
+        {
+            var player = new Player() {Name = "Player 1"};
+            var gameRoom = new GameRoom() {Name = "Game Room 1"};
+            var gameRoomPlayer = new GameRoomPlayer()
+            {
+                GameRoom = gameRoom,
+                Player = player,
+                IsHost = isInRoom,
+                IsBanned = isBanned,
+                IsInRoom = isInRoom,
+            };
+
+            await _context.AddAsync(player);
+            await _context.AddAsync(gameRoom);
+            await _context.AddAsync(gameRoomPlayer);
+            await _context.SaveChangesAsync();
+            
+            return Tuple.Create(gameRoom, player, gameRoomPlayer);
         }
     }
 }
