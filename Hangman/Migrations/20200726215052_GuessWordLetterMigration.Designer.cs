c749fb55b59ca5f991aa909b9edfaac12136ae76
IgooorGP <igor.feq@gmail.com>
2020-07-26 18:53:37 -0300

    Added GuessWord and GuessLetter relations. Added new migration.

--- Hangman/Migrations/20200726215052_GuessWordLetterMigration.Designer.cs
+++ Hangman/Migrations/20200726215052_GuessWordLetterMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Hangman.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Hangman.Migrations
 {
     [DbContext(typeof(HangmanDbContext))]
-    partial class HangmanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200726215052_GuessWordLetterMigration")]
+    partial class GuessWordLetterMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Hangman/Models/GameRoom.cs
+++ Hangman/Models/GameRoom.cs
@@ -1,15 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace Hangman.Models
 {
     public class GameRoom : BaseEntity
     {
-        [Required] [MaxLength(255)] public string Name { get; set; } = null!;
+        [Required] 
+        [MaxLength(255)] 
+        public string Name { get; set; } = null!;
         
         // fk to join table for many-to-many relationships to access Users
         public ICollection<GameRoomPlayer> GameRoomPlayers { get; set; } = null!;
+        
+        // one-to-many
+        public ICollection<GuessWord> GuessWords { get; set; } = null!;
+    }
+    
+    public class GuessWord : BaseEntity
+    {
+        [Required]
+        public GameRoom GameRoom { get; set; } = null!;
+
+        [Required] 
+        public Guid GameRoomId { get; set; }
+        
+        public ICollection<GuessLetter> GuessLetters { get; set; } = null!;
+    }
+    
+    public class GuessLetter : BaseEntity
+    {
+        [Required]
+        public GuessWord GuessWord { get; set; } = null!;
+
+        [Required]
+        public Guid GuessWordId { get; set; }
     }
     
     public class Player : BaseEntity
@@ -19,6 +45,8 @@ namespace Hangman.Models
         public string Name { get; set; } = null!;
 
         // fk to join table for many-to-many relationships to access GameRooms
+        // ignored on jsons: past of a user may bring too many results
+        [JsonIgnore]
         public ICollection<GameRoomPlayer> GameRoomPlayers { get; set; } = null!;
     }
     
@@ -44,5 +72,5 @@ namespace Hangman.Models
         
         [Required]
         public bool IsInRoom { get; set; }
-    } 
+    }
 }
