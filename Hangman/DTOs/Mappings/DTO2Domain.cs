eb2f8df3f3880fcc64255864441755224d412a47
IgooorGP <igor.feq@gmail.com>
2020-08-15 14:36:18 -0300

    Added AutoMapper profiles for layers communication

---
+++ Hangman/DTOs/Mappings/DTO2Domain.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Hangman.Models;
+
+namespace Hangman.DTOs.Mappings
+{
+    public class DTO2Domain : Profile
+    {
+        public DTO2Domain()
+        {
+            CreateMap<GameRoomDTO, GameRoom>();
+        }
+    }
+}
---
+++ Hangman/DTOs/Mappings/Domain2DTO.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace Hangman.DTOs.Mappings
+{
+    public class Domain2DTO : Profile
+    {
+        public Domain2DTO()
+        {
+            // TODO: implement response-like DTOs
+        }
+    }
+}
